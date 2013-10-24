4ea248b5936ba5818d19cf6443bacb5f3be2f201
AleMarra <alemarra@gmail.com>
2013-10-24 00:54:51 -0300

    Cambia la ruta de acceso root, userController pasa a ser el path del controlador

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -22,7 +22,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
-@Path("/")
+@Path("/userController")
 public class UserController {
 
 	private Map<String, User> users;
